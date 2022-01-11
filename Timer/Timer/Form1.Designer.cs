6c906f65cd90574f2b11abc87e9b5f0446e24e38
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-01-11 10:29:17 +0100

    Add files via upload

---
+++ Timer/Timer/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Timer
+{
+    partial class Vrijeme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelVrijeme = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelVrijeme
+            // 
+            this.labelVrijeme.AutoSize = true;
+            this.labelVrijeme.Location = new System.Drawing.Point(216, 187);
+            this.labelVrijeme.Name = "labelVrijeme";
+            this.labelVrijeme.Size = new System.Drawing.Size(86, 13);
+            this.labelVrijeme.TabIndex = 0;
+            this.labelVrijeme.Text = "Trenutno vrijeme";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Vrijeme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelVrijeme);
+            this.Name = "Vrijeme";
+            this.Text = "Vrijeme";
+            this.Load += new System.EventHandler(this.Vrijeme_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVrijeme;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
